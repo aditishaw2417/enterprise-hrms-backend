283dd0317061cf381e0cf72021b7c658fe461eb5
Aditi Shaw <aditishaw2417@gmail.com>
2026-03-03 18:02:27 +0530

    added validation and get by role api

--- src/main/java/com/aditi/hrms/demo/controller/RoleController.java
+++ src/main/java/com/aditi/hrms/demo/controller/RoleController.java
@@ -7,8 +7,17 @@ import com.aditi.hrms.demo.dto.role.CreateRoleRequest;
 import com.aditi.hrms.demo.dto.role.RoleResponse;
 import com.aditi.hrms.demo.model.role.Role;
 import com.aditi.hrms.demo.service.role.RoleService;
+
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
 
 
 @RestController
@@ -19,9 +28,14 @@ public class RoleController {
         this.roleService=roleService;
     }
     @PostMapping
-    public RoleResponse createRole(@RequestBody CreateRoleRequest request) {
+    public RoleResponse createRole(@Valid @RequestBody CreateRoleRequest request) {
         Role role = roleService.createRole(request.getRoleName(), request.getDescription());
         return new RoleResponse(role.getId(), role.getRoleName());
     }
     
+    @GetMapping("/{name}")
+    public RoleResponse getRole(@PathVariable String name){
+        Role role = roleService.getByRoleName(name);
+        return new RoleResponse(role.getId(), role.getRoleName());
+    }
 }
--- src/main/java/com/aditi/hrms/demo/dto/role/CreateRoleRequest.java
+++ src/main/java/com/aditi/hrms/demo/dto/role/CreateRoleRequest.java
@@ -1,6 +1,9 @@
 package com.aditi.hrms.demo.dto.role;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class CreateRoleRequest {
+    @NotBlank
     private String roleName;
     private String description;
     public String getRoleName(){
